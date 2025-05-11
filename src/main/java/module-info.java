180e50390b8d84a7c2b41c4075357756d40b5923
Aryan Babber <86878236+AryanBabber@users.noreply.github.com>
2025-05-11 05:56:52 +0530

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.fileencryptor {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.xml;
-
-
-    opens com.example.fileencryptor to javafx.fxml;
-    exports com.example.fileencryptor;
-}
